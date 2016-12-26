c0dddb72a473474e6252306aa18505aa0adc11c6
slapab <sla.pab.dev@gmail.com>
2016-12-26 22:28:48 +0100

    Updated pseudorndomdotsanimation

--- src/PseudoRandomDotsAnimation.hpp
+++ src/PseudoRandomDotsAnimation.hpp
@@ -1,12 +1,7 @@
-/*
- * PseudoRandomDotsAnimation.hpp
- *
- *  Created on: 24.12.2016
- *      Author: scott
- */
-
 #ifndef PSEUDORANDOMDOTSANIMATION_HPP_
 #define PSEUDORANDOMDOTSANIMATION_HPP_
+
+#include "AnimationInterface.h"
 
 namespace animations {
 class PseudoRandomDotsAnimation final : public AnimationInterface {
@@ -28,43 +23,100 @@ inline void PseudoRandomDotsAnimation::run(AnimationContext* context) {
 
 
     // generate index
-    for (int j = 0; j < 2; ++j ) {
-    for (int i = 0; i < 298; ++i) {
+    for (int j = 0; j < 3; ++j ) {
+    for (int i = 0; i < 400; ++i) {
+        uint16_t idx;
+        do {
+        idx = rndGen.randomize() & 0x01FF;
+        } while(idx >= LEDS_COUNT-1);
 
-        uint16_t idx = rndGen.randomize() % (LEDS_COUNT-1);
-
-        fg.r = rndGen.randomize() ;
-        fg.g = rndGen.randomize();
+        fg.r = rndGen.randomize() % 230;
+        fg.g = rndGen.randomize() % 200;
         fg.b = rndGen.randomize() % 120;
 
         m_LedStrip.fill(idx, idx+1, fg);
         while (false == m_LedStrip.send());
 
+        m_Timer.sleep(10);
+        fg.r = 0;
+        fg.g = 0;
+        fg.b = 0;
 
-            m_Timer.sleep(20);
-            fg.r = 0;
-            fg.g = 0;
-            fg.b = 0;
-            idx = rndGen.randomize() % (LEDS_COUNT-1);
-            m_LedStrip.fill(idx, idx+1, fg);
+
+        do {
+        idx = rndGen.randomize() & 0x01FF;
+        } while(idx >= LEDS_COUNT-1);
+
+        m_LedStrip.fill(idx, idx+1, fg);
+        while (false == m_LedStrip.send());
+
+
+        m_Timer.sleep(15);
+    }
+        //-----------------------------------------------
+        // just turn off most leds
+//        fg = {0,0,0};
+//        for (std::size_t i = 0; i < 400; ++i) {
+//            uint16_t idx ;//= rndGen.randomize();
+
+//            do {
+//                idx = rndGen.randomize() & 0x01FF;
+//            } while(idx >= LEDS_COUNT-1);
+
+//            m_LedStrip.fill(idx, idx+1, fg);
+//            while (false == m_LedStrip.send());
+//            m_Timer.sleep(15);
+//        }
+        //-----------------------------------------------
+
+        //-----------------------------------------------
+        // flash light - as clear strip
+        uint16_t idx;
+        do {
+            idx = rndGen.randomize() & 0x01FF;
+        } while (idx >= LEDS_COUNT-1);
+
+        // bright up
+        RGBColor c{5,5,5};
+        for (auto i = 0; i <= 180; i += 5) {
+            m_LedStrip.fill(0, LEDS_COUNT-1, c);
             while (false == m_LedStrip.send());
+            m_Timer.sleep(8);
+            if (j < 1) {
+                c.r = c.g = c.b = static_cast<uint8_t>(i);
+            } else if (j < 2) {
+                c.r = c.b = 0;
+                c.g = static_cast<uint8_t>(i);
+            } else if (j < 3) {
+                c.r = c.g = 0;
+                c.b = static_cast<uint8_t>(i);
+            }
+        }
 
+        // bright down
+        for (int32_t i = 180; i >= 0; i -= 5) {
+            m_LedStrip.fill(0, LEDS_COUNT, c);
+            while (false == m_LedStrip.send());
+            m_Timer.sleep(8);
+            if (j < 1) {
+                c.r = c.g = c.b = static_cast<uint8_t>(i);
+            } else if (j < 2) {
+               c.r = c.b = 0;
+               c.g = static_cast<uint8_t>(i);
+            } else if (j < 3) {
+                c.r = c.g = 0;
+                c.b = static_cast<uint8_t>(i);
+            }
+        }
 
-        m_Timer.sleep(20);
+        // turn off leds
+        c = {0,0,0};
+        m_LedStrip.fill(0, LEDS_COUNT, c);
+        while (false == m_LedStrip.send());
+        m_Timer.sleep(500);
 
-        // srnad
-        rndGen.seed(rndGen.randomize(), idx, fg.b);
+        //-----------------------------------------------
     }
-    }
-
-
-//    for (int i = 0 ; i < 100; ++i) {
-//
-//        m_LedStrip.fill(idx, idx+1, fg);
-//        while (false == m_LedStrip.send());
-//
-//        m_Timer.sleep(50);
-//    }
 
 }
 
